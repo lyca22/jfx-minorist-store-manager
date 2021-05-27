f4321aae8451f059222cc33a618ad78f28e0830c
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-05-27 10:00:02 -0500

    Images added in the choose-user-type.fxml file

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,17 +2,21 @@ package ui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.MinoristStore;
 
 public class Main extends Application {
 
+	private MinoristStore store;
+	private MinoristStoreGUI minoristStoreGUI;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
