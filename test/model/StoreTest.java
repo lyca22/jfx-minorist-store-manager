d051506200e06ba5c2c424b7119b5e8095cc2715
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-06-11 01:55:58 -0500

    Conflict resolved and added more tests

--- test/model/StoreTest.java
+++ test/model/StoreTest.java
@@ -2,7 +2,6 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,9 +22,7 @@ class StoreTest {
 		Product testProduct = new Product(82498429, "iPhone 11", testCategory, "Apple", 2300000, 10, "This is a phone");
 		testProduct.getSellerList().add(testSeller);
 		minoristStore.addProduct(testProduct);
-
 	}
-
 
 	public void setupScenary2() {
 		minoristStore = new MinoristStore();
@@ -47,19 +44,18 @@ class StoreTest {
 		testProduct2.getSellerList().add(testSeller2);
 		minoristStore.addProduct(testProduct2);
 
-		List<Product> productList = new ArrayList<Product>();
+		ArrayList<Product> productList = new ArrayList<Product>();
 		productList.add(testProduct2);
-		List<Integer> quantity = new ArrayList<Integer>();
+		ArrayList<Integer> quantity = new ArrayList<Integer>();
 		quantity.add(1);
 
 		Consumer testClient = new Consumer("ArielP", "abcd", "Ariel", "Pabon", 234567547, "Cr 78 # 2-21");
 		minoristStore.addConsumerAccount("ArielP", "abcd", "Ariel", "Pabon", 234567547, "Cr 78 # 2-21");
+		
 		PaymentMethod paymentMethod = new PaymentMethod("BBVA", PaymentType.CARD);
 		PaymentInformation paymentInfo = new PaymentInformation(paymentMethod, 123456);
-		LocalDateTime date = LocalDateTime.now();
-		List<Order> orderList = new ArrayList<Order>();
-		Order testOrder = new Order(1234567, date, testClient, OrderState.REQUESTED, paymentInfo); //I changed the Order class constructor, check it out.
-		orderList.add(testOrder); //AGREGAR CUANDO ESTE EL METODO*/
+		
+		minoristStore.addOrder(testClient, paymentInfo, productList, quantity);
 	}
 
 
@@ -74,9 +70,6 @@ class StoreTest {
 		product.getSellerList().add(testSeller);
 		minoristStore.addProduct(product);
 
-		//boolean added = minoristStore.addProduct(product);  testSeller.getProductList(); 
-		//assertTrue(added);  
-
 		List<Product> productList = minoristStore.getGeneralProductList();
 		assertEquals(2, productList.size());
 	}
@@ -84,8 +77,63 @@ class StoreTest {
 	@Test
 	public void testAddProduct2() {
 		setupScenary2();
+		
+		Seller testSeller2 = new Seller("official-LG", "2468", "LG");
+		Category testCategory = new Category("Electronic devices");
+			
+		Product product = new Product(135790, "LG OLED UHD", testCategory, "LG", 6400000, 3, "This is a TV");
+		product.getSellerList().add(testSeller2);
+		minoristStore.addProduct(product);
+
+		List<Product> productList = minoristStore.getGeneralProductList();
+		assertEquals(3, productList.size());
 	}
 
+
+	//Add request correctly
+	@Test
+	public void testAddRequest() {
+		setupScenary1();
+
+		Seller testSeller = new Seller("apple", "12345", "Apple Inc.");
+		minoristStore.addSellerAccount("apple", "12345", "Apple Inc.");
+
+		Category testCategory = new Category("Electronic devices");
+		minoristStore.addCategory("Electronic devices");
+
+		Product testProduct = new Product(82498411, "iPhone 6s", testCategory, "Apple", 1300000, 3, "This is a phone");
+		testProduct.getSellerList().add(testSeller);
+		minoristStore.addProduct(testProduct);
+
+		RequestType requestType = RequestType.ADD;  
+
+		minoristStore.addRequest(82498411, "iPhone 6s", testCategory, "Apple", 1300000, 3, "This is a phone", testSeller, requestType);
+
+		List<Request> requestList = minoristStore.getRequestList();
+		assertEquals(1, requestList.size());
+	}
+
+	@Test
+	public void testAddRequest2() {
+		setupScenary2();
+
+		Seller testSeller = new Seller("official-LG", "2468", "LG");
+		minoristStore.addSellerAccount("official-LG", "2468", "LG");
+
+		Category testCategory = new Category("Home Appliances");
+		minoristStore.addCategory("Home Appliances");
+
+		Product testProduct = new Product(13468411, "Fridge", testCategory, "LG", 5250000, 4, "This is a fridge");
+		testProduct.getSellerList().add(testSeller);
+		minoristStore.addProduct(testProduct);
+
+		RequestType requestType = RequestType.ADD;  
+
+		minoristStore.addRequest(13468411, "Fridge", testCategory, "LG", 5250000, 4, "This is a fridge", testSeller, requestType);
+
+		List<Request> requestList = minoristStore.getRequestList();
+		assertEquals(1, requestList.size());
+	}
 
 
 	//Add account correctly
@@ -114,11 +162,10 @@ class StoreTest {
 		boolean added = minoristStore.addSellerAccount(username, password, tradeName);
 
 		assertTrue(added);
-
 	}
+	
 
-
-	//Can�t add an account
+	//Can't add an account
 	@Test
 	public void testCantAddAccount() {
 		setupScenary1();
@@ -146,33 +193,6 @@ class StoreTest {
 	}
 
 
-	//Add order correctly
-	@Test
-	public void testAddOrder() {
-		setupScenary1();
-
-		/*long iD = 2467893;
-		LocalDateTime date = LocalDateTime.now();
-		Consumer client = new Consumer("JoseP", "abcd", "Jose", "Perez", 234567547, "Cr 66 # 2-21");
-		List<Product> productList = new ArrayList<Product>();
-		List<Integer> quantity = new ArrayList<Integer>();
-		int price = 2300000;
-		OrderState orderState = OrderState.SENT;
-		PaymentMethod paymentMethod = new PaymentMethod("PayPal", PaymentType.ONLINE_SYSTEM);
-		PaymentInformation paymentInfo = new PaymentInformation(paymentMethod, 123456);
-
-
-		Order testOrder = new Order;
-		List<Order> orderList = new ArrayList<Order>();
-		orderList.add(testOrder);*/
-	}
-
-	@Test
-	public void testAddOrder2() {
-		setupScenary2();
-	}
-
-
 	//Add category correctly
 	@Test
 	public void testAddCategory() {
@@ -194,56 +214,8 @@ class StoreTest {
 		assertTrue(added);
 	}
 
-
-	//Add request correctly
-	@Test
-	public void testAddRequest() {
-		setupScenary1();
-
-		Seller testSeller = new Seller("apple", "12345", "Apple Inc.");
-		minoristStore.addSellerAccount("apple", "12345", "Apple Inc.");
-
-		Category testCategory = new Category("Electronic devices");
-		minoristStore.addCategory("Electronic devices");
-
-		Product testProduct = new Product(82498411, "iPhone 6s", testCategory, "Apple", 1300000, 3, "This is a phone");
-		testProduct.getSellerList().add(testSeller);
-		minoristStore.addProduct(testProduct);
-
-		RequestType requestType = RequestType.ADD;  
-
-		minoristStore.addRequest(82498411, "iPhone 6s", testCategory, "Apple", 1300000, 3, "This is a phone", testSeller, requestType);
-
-		List<Request> requestList = minoristStore.getRequestList();
-		assertEquals(1, requestList.size());
-
-	}
-
-	@Test
-	public void testAddRequest2() {
-		setupScenary2();
-
-		Seller testSeller = new Seller("official-LG", "2468", "LG");
-		minoristStore.addSellerAccount("official-LG", "2468", "LG");
-
-		Category testCategory = new Category("Home Appliances");
-		minoristStore.addCategory("Home Appliances");
-
-		Product testProduct = new Product(13468411, "Fridge", testCategory, "LG", 5250000, 4, "This is a fridge");
-		testProduct.getSellerList().add(testSeller);
-		minoristStore.addProduct(testProduct);
-
-		RequestType requestType = RequestType.ADD;  
-
-		minoristStore.addRequest(13468411, "Fridge", testCategory, "LG", 5250000, 4, "This is a fridge", testSeller, requestType);
-
-		List<Request> requestList = minoristStore.getRequestList();
-		assertEquals(1, requestList.size());
-	}
 	
 	//Add payment method
-	//Can't remove an account
-	
 	@Test
 	public void testAddPaymentmethod() {
 		setupScenary1();
@@ -252,7 +224,6 @@ class StoreTest {
 		PaymentType type = PaymentType.ONLINE_SYSTEM;
 		boolean added = minoristStore.addPaymentMethod(name, type);
 		assertTrue(added);
-
 	}
 
 	@Test
@@ -264,32 +235,130 @@ class StoreTest {
 		boolean added = minoristStore.addPaymentMethod(name, type);
 		assertTrue(added);
 	}
-	//Edit a product
-	//Can't remove a category
+
 	
+	//Add order correctly
 	@Test
-	public void testEditProduct() {
+	public void testAddOrder() {
 		setupScenary1();
 
-
-		/*long iD = 82498429;
-		String name = "iPhone 11";
+		Consumer client = new Consumer("JoseP", "abcd", "Jose", "Perez", 234567547, "Cr 66 # 2-21");
 		Category testCategory = new Category("Electronic devices");
-		String brand = "Apple"; 
-		int price = 1950000;
-		int stock = 7;
-		String description = "This is a phone";*/
+		Product testProduct = new Product(82498429, "iPhone 11", testCategory, "Apple", 2300000, 10, "This is a phone");
+		
+		ArrayList<Product> productList = new ArrayList<Product>();
+		productList.add(testProduct);
+		ArrayList<Integer> quantity = new ArrayList<Integer>();
+		quantity.add(1);
+		
+		PaymentMethod paymentMethod = new PaymentMethod("PayPal", PaymentType.ONLINE_SYSTEM);
+		PaymentInformation paymentInfo = new PaymentInformation(paymentMethod, 123456);
 
-
-
+		minoristStore.addOrder(client, paymentInfo, productList, quantity);
+		
+		List<Order> orderList = minoristStore.getOrderList();
+		assertEquals(1, orderList.size());
 	}
 
 	@Test
-	public void testEditProduct2() {
+	public void testAddOrder2() {
 		setupScenary2();
+		
+		Consumer client = new Consumer("LuisR", "abcd", "Luis", "Reyes", 234567547, "Cr 66 # 2-21");
+		Category testCategory = new Category("Home Appliances");
+		Product testProduct = new Product(56789012, "WashTowers", testCategory, "LG", 1890000, 4,  "This is a washin machine");
+		
+		ArrayList<Product> productList = new ArrayList<Product>();
+		productList.add(testProduct);
+		ArrayList<Integer> quantity = new ArrayList<Integer>();
+		quantity.add(2);
+		
+		PaymentMethod paymentMethod = new PaymentMethod("PayPal", PaymentType.ONLINE_SYSTEM);
+		PaymentInformation paymentInfo = new PaymentInformation(paymentMethod, 123456);
+
+		minoristStore.addOrder(client, paymentInfo, productList, quantity);
+		
+		List<Order> orderList = minoristStore.getOrderList();
+		assertEquals(2, orderList.size());
+	
 	}
 
 
+	//Search account
+	@Test
+	public void testSearchAccount() {
+		setupScenary1();
+
+		String username = "apple";
+		Seller testSeller = new Seller("apple", "12345", "Apple Inc.");
+		Account found = minoristStore.searchAccount(username);
+
+		assertEquals(testSeller.getUsername(), found.getUsername());
+	}
+
+	@Test
+	public void testSearchAccount2() {
+		setupScenary2();
+
+		String username = "official-LG";
+		Seller testSeller = new Seller("official-LG", "2468", "LG");
+		Account found = minoristStore.searchAccount(username);
+
+		assertEquals(testSeller.getUsername(), found.getUsername());
+	}
+
+
+	//Search category
+	@Test
+	public void testSearchCategory() {
+		setupScenary1();
+
+		String name = "Electronic devices";
+		Category testCategory = new Category("Electronic devices");
+		Category found = minoristStore.searchCategory(name);
+
+		assertEquals(testCategory.getName(), found.getName());
+	}
+
+	@Test
+	public void testSearchCategory2() {
+		setupScenary2();
+
+		String name = "Home Appliances";
+		Category testCategory = new Category("Home Appliances");
+		Category found = minoristStore.searchCategory(name);
+
+		assertEquals(testCategory.getName(), found.getName());
+	}
+
+
+	//Search product
+	@Test
+	public void testSearchProduct() {
+		setupScenary1();
+
+		long id = 82498429;
+		Category testCategory = new Category("Electronic devices");
+		Product testProduct = new Product(82498429, "iPhone 11", testCategory, "Apple", 2300000, 10, "This is a phone");
+
+		Product found = minoristStore.searchProduct(id);
+
+		assertEquals(testProduct.getID(), found.getID());
+	}
+
+	@Test
+	public void testSearchProduct2() {
+		setupScenary2();
+
+		long id = 56789012;
+		Category testCategory = new Category("Home Appliances");
+		Product testProduct = new Product(56789012, "WashTowers", testCategory, "LG", 1890000, 4,  "This is a washin machine");
+
+		Product found = minoristStore.searchProduct(id);
+
+		assertEquals(testProduct.getID(), found.getID());
+	}
+	
 
 	//Edit a category
 	@Test
@@ -317,76 +386,17 @@ class StoreTest {
 
 	//Edit payment method
 	@Test
-	public void testEditPaymentMethod() {
-		setupScenary1();
-
-	}
-
-	@Test
 	public void testEditPaymentMethod2() {
 		setupScenary2();
+		
+		PaymentMethod paymentMethod = new PaymentMethod("BBVA", PaymentType.CARD);
+		String newName = "Amazon Pay"; 
+		PaymentType newPaymentType = PaymentType.ONLINE_SYSTEM; 
 
+		boolean edited = minoristStore.editPaymentMethod(paymentMethod, newName, newPaymentType);
+
+		assertTrue(edited);
 	}
 
-
-
-	//Search account
-	@Test
-	public void testSearchAccount() {
-		setupScenary1();
-
-		String username = "apple";
-		Seller testSeller = new Seller("apple", "12345", "Apple Inc.");
-		Account found = minoristStore.searchAccount(username);
-
-		assertEquals(testSeller.getUsername(), found.getUsername());
-	}
-
-	@Test
-	public void testSearchAccount2() {
-		setupScenary2();
-
-		String username = "official-LG";
-		Seller testSeller = new Seller("official-LG", "2468", "LG");
-		Account found = minoristStore.searchAccount(username);
-
-		assertEquals(testSeller.getUsername(), found.getUsername());
-	}
-	
-
-	//Search category
-	@Test
-	public void testSearchCategory() {
-		setupScenary1();
-	}
-
-	@Test
-	public void testSearchCategory2() {
-		setupScenary2();
-	}
-
-
-	//Search product
-	@Test
-	public void testSearchProduct() {
-		setupScenary1();
-	}
-
-	@Test
-	public void testSearchProduct2() {
-		setupScenary2();
-	}
-
-	
-	//Search payment method
-	@Test
-	public void testSearchPaymentMethod() {
-		setupScenary1();
-	}
-
-	@Test
-	public void testSearchPaymentMethod2() {
-		setupScenary2();
-	}
 
 }
